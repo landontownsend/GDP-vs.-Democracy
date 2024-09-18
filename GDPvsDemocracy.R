4c271fc302a1b0dc813e0c9beaf7b46f48a9c6c4
landontownsend <landon.townsend@icloud.com>
2024-09-17 22:25:53 -0400

    GDPvsDemocracy.R

--- GDPvsDemocracy.R
+++ GDPvsDemocracy.R
@@ -13,15 +13,17 @@ ggplot(merged_data, aes(x = NY.GDP.PCAP.CD, y = polity)) + geom_point(color = "b
 cleaned_data <- na.omit(merged_data[, c("NY.GDP.PCAP.CD", "polity")])
 # Plotting data and enhancing chart for efficacy
 ggplot(cleaned_data, aes(x = NY.GDP.PCAP.CD, y = polity)) +
-  +     geom_point(color = "steelblue", size = 2, alpha = 0.7) +  
-  +     scale_x_log10(labels = comma) +                                          
-  +     geom_smooth(method = "lm", color = "red", se = FALSE) +     # Linear trendline
-  +     labs(x = "GDP per Capita", y = "Democracy Score", 
-             +          title = "GDP vs. Democracy", 
-             +          subtitle = "Exploring the Relationship Between GDP per Capita and Democracy Scores",
-             +          caption = "Source: World Development Indicators & Polity IV") + scale_y_continuous(breaks = seq(-75, 100, by = 20)) +   theme_minimal() +                                           
-  +     theme(plot.title = element_text(face = "bold", size = 14), 
-              +           plot.subtitle = element_text(size = 10, face = "italic"),  
-              +           plot.caption = element_text(face = "italic", size = 8),    
-              +           axis.text = element_text(size = 10),                       
-              +           axis.title = element_text(face = "bold")) 
+  geom_point(color = "steelblue", size = 2, alpha = 0.7) +  
+  scale_x_log10(labels = comma) +                                          
+  geom_smooth(method = "lm", color = "red", se = FALSE) +  # Linear trendline
+  labs(x = "GDP per Capita", y = "Democracy Score", 
+       title = "GDP vs. Democracy", 
+       subtitle = "Exploring the Relationship Between GDP per Capita and Democracy Scores",
+       caption = "Source: World Development Indicators & Polity IV") + 
+  scale_y_continuous(breaks = seq(-75, 100, by = 20)) +  
+  theme_minimal() +                                           
+  theme(plot.title = element_text(face = "bold", size = 14), 
+        plot.subtitle = element_text(size = 10, face = "italic"),  
+        plot.caption = element_text(face = "italic", size = 8),    
+        axis.text = element_text(size = 10),                       
+        axis.title = element_text(face = "bold"))
